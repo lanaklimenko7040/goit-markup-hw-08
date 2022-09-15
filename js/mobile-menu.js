c5d63453cf85e6c29ee7a409d60bec77ce38aa3e
Klymenko Svetlana <lana.klimenko7040@gmail.com>
2022-09-15 14:40:22 +0300

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,27 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open'),
-    closeMenuBtn: document.querySelector('.menu-close'),
-     menu: document.querySelector('.mob-menu'),
-     menu: document.querySelector('.body')
+    openMenuBtn: document.querySelector(".menu-open"),
+    closeMenuBtn: document.querySelector(".menu-close"),
+    menu: document.querySelector(".mob-menu"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("no-scroll");
   }
 })();
+
+
+
+
+
+
+
+
+
+
+
+
